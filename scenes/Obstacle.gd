a7ee09e32eb135287d2c75f495b6f9dfeeadd770
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-03-04 15:08:50 +0700

    feat: modify level 2

--- scenes/Obstacle.gd
+++ scenes/Obstacle.gd
@@ -1,8 +1,15 @@
 extends RigidBody2D
 
-@export var sceneName = "LoseScreen"
 @onready var banana_sprite = $Banana
+@onready var apple_sprite = $Apple
 @onready var animation_player = $AnimationPlayer
 
 func _ready():
-	animation_player.play("banana")
+	#animation_player.play("banana")
+	if get_tree().current_scene.name == "Level1":
+		animation_player.play("banana")
+		apple_sprite.visible = false
+			
+	if get_tree().current_scene.name == "Level2":
+		animation_player.play("apple")
+		banana_sprite.visible =  false
